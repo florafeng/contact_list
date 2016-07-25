3c01050b9ea4eda5f14574c12e0dc52d99b72932
florafeng <sfeng3@ualberta.ca>
2016-07-25 18:21:59 -0400

    Main menu

--- contact_list.rb
+++ contact_list.rb
@@ -5,4 +5,22 @@ class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+  def start
+    if ARGV.empty?
+      puts "Here is a list of available commands:"
+      puts "\tnew - Create a new contact"
+      puts "\tlist - List all contacts"
+      puts "\tshow - Show a contact"
+      puts "\tfind - Find a contact"
+    else
+      case @command
+      when "new" then create_new_contact
+      when "list"
+      when "show"
+      when "search"
+      end
+    end
+  end
 end
+
+ContactList.new.start
